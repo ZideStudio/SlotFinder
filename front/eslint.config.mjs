ec82dc292e5f56cca95cd14d807dae1032eeae4a
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2025-09-19 10:39:58 +0200

    refactor: remove Playwright integration and related configurations

--- front/eslint.config.mjs
+++ front/eslint.config.mjs
@@ -5,8 +5,6 @@ export default defineConfig({
   enablePrettier: 'on',
   enableVitest: true,
   unitTestFiles: ['src/**/*.{test,spec,steps}.{js,jsx,ts,tsx}'],
-  enablePlaywright: true,
-  unitE2eFiles: ['tests/**/*.{test,spec}.{js,jsx,ts,tsx}'],
   extendConfig: [
     {
       files: ['mocks/**/*'],
