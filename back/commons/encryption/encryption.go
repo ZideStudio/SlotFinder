e88900f15841b2d15d7be307fcdcc3fa9fcf2638
Nekeo <nekeo@zide.fr>
2025-09-20 19:38:40 +0200

    refactor: encryption methods

--- back/commons/encryption/encryption.go
+++ back/commons/encryption/encryption.go
@@ -3,48 +3,69 @@ package encryption
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
 	"encoding/base64"
+	"errors"
+	"io"
 	"os"
 )
 
-var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
-
-var secret = os.Getenv("SECURED_STR_KEY")
-
-// Encrypt
-func encode(b []byte) string {
-	return base64.StdEncoding.EncodeToString(b)
+func getKey() ([]byte, error) {
+	key := []byte(os.Getenv("ENCRYPTION_KEY"))
+	switch len(key) {
+	case 16, 24, 32:
+		return key, nil
+	default:
+		return nil, errors.New("ENCRYPTION_KEY must be 16, 24, or 32 bytes")
+	}
 }
 
 func Encrypt(text string) (string, error) {
-	block, err := aes.NewCipher([]byte(secret))
+	key, err := getKey()
 	if err != nil {
 		return "", err
 	}
-	plainText := []byte(text)
-	ctr := cipher.NewCTR(block, bytes)
-	cipherText := make([]byte, len(plainText))
-	ctr.XORKeyStream(cipherText, plainText)
-	return encode(cipherText), nil
-}
 
-// Decrypt
-func decode(s string) []byte {
-	data, err := base64.StdEncoding.DecodeString(s)
+	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	return data
+
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	ctr := cipher.NewCTR(block, iv)
+	cipherText := make([]byte, len(text))
+	ctr.XORKeyStream(cipherText, []byte(text))
+
+	return base64.StdEncoding.EncodeToString(append(iv, cipherText...)), nil
 }
 
 func Decrypt(text string) (string, error) {
-	block, err := aes.NewCipher([]byte(secret))
+	key, err := getKey()
 	if err != nil {
 		return "", err
 	}
-	cipherText := decode(text)
-	ctr := cipher.NewCTR(block, bytes)
+
+	data, err := base64.StdEncoding.DecodeString(text)
+	if err != nil {
+		return "", err
+	}
+	if len(data) < aes.BlockSize {
+		return "", errors.New("ciphertext too short")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	iv, cipherText := data[:aes.BlockSize], data[aes.BlockSize:]
+	ctr := cipher.NewCTR(block, iv)
 	plainText := make([]byte, len(cipherText))
 	ctr.XORKeyStream(plainText, cipherText)
+
 	return string(plainText), nil
 }
