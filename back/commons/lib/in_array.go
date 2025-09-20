7d5564db154d961831c7f93d4dc29e44174ff7a2
Nekeo <nekeo@zide.fr>
2025-09-20 12:45:36 +0200

    refactor: in array method

--- back/commons/lib/in_array.go
+++ back/commons/lib/in_array.go
@@ -1,19 +1,10 @@
 package lib
 
-import (
-	"reflect"
-)
-
-func InArray(val any, array any) (index int) {
-	values := reflect.ValueOf(array)
-
-	if reflect.TypeOf(array).Kind() == reflect.Slice || values.Len() > 0 {
-		for i := 0; i < values.Len(); i++ {
-			if reflect.DeepEqual(val, values.Index(i).Interface()) {
-				return i
-			}
+func InArray[T comparable](val T, array []T) int {
+	for i, v := range array {
+		if v == val {
+			return i
 		}
 	}
-
 	return -1
 }
