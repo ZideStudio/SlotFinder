7ee531bf3930404412335e4662d0b24fbdd8f97b
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2025-09-26 15:07:50 +0200

    fix(mocks): update account handlers to use environment variable for URL

--- front/mocks/handlers/accountHandlers.ts
+++ front/mocks/handlers/accountHandlers.ts
@@ -1,13 +1,13 @@
 import { account, accountError } from '@Mocks/fixtures/accountFixtures';
 import { delay, http, HttpResponse } from 'msw';
 
-export const postAccount201 = http.post(`http://localhost:3005/v1/account`, async () => {
+export const postAccount201 = http.post(`${import.meta.env.FRONT_BACKEND_URL}/v1/account`, async () => {
   await delay();
 
   return HttpResponse.json(account, { status: 201 });
 });
 
-export const postAccount400 = http.post(`http://localhost:3005/v1/account`, async () => {
+export const postAccount400 = http.post(`${import.meta.env.FRONT_BACKEND_URL}/v1/account`, async () => {
   await delay();
 
   return HttpResponse.json(accountError, { status: 400 });
