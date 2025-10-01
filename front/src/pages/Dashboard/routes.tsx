c8869572fed8be5b9b905211ed47d46f6fca0336
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2025-10-01 11:00:34 +0200

    feat(front): create dashboard page

---
+++ front/src/pages/Dashboard/routes.tsx
@@ -0,0 +1,7 @@
+import type { RouteObject } from 'react-router-dom';
+import { Dashboard } from './Dashboard';
+
+export const dashboardRoutes: RouteObject = {
+  path: 'dashboard',
+  element: <Dashboard />,
+};
--- front/src/routing/routes.tsx
+++ front/src/routing/routes.tsx
@@ -1,5 +1,6 @@
 import { Layout } from '@Front/components/Layout';
 import { authenticationRoutes } from '@Front/pages/Authentication';
+import { dashboardRoutes } from '@Front/pages/Dashboard';
 import { errorRoutes } from '@Front/pages/Error';
 import { homeRoutes } from '@Front/pages/Home';
 import { oauthCallbackRoutes } from '@Front/pages/OAuthCallback';
@@ -9,6 +10,6 @@ export const routeObject: RouteObject[] = [
   {
     path: '/',
     element: <Layout />,
-    children: [homeRoutes, authenticationRoutes, oauthCallbackRoutes, errorRoutes],
+    children: [homeRoutes, dashboardRoutes, authenticationRoutes, oauthCallbackRoutes, errorRoutes],
   },
 ];
