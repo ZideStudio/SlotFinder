d8c946130a6b6318408543a23a31317578e70caf
Nekeo <nekeo@zide.fr>
2025-10-04 18:14:45 +0200

    refactor: set cookie

--- back/commons/lib/cookie.go
+++ back/commons/lib/cookie.go
@@ -1,19 +1,28 @@
 package lib
 
 import (
+	"app/commons/constants"
 	"app/config"
 
 	"github.com/gin-gonic/gin"
 )
 
-func RemoveCookie(c *gin.Context) {
+func setCookie(c *gin.Context, token string, expiration int) {
 	c.SetCookie(
 		"access_token",            // name
-		"",                        // value
-		-1,                        // max age in seconds
+		token,                     // value
+		expiration,                // max age in seconds
 		"/",                       // path
 		config.GetConfig().Domain, // domain
 		true,                      // secure
 		true,                      // httpOnly
 	)
+}
+
+func AddCookie(c *gin.Context, token string) {
+	setCookie(c, token, constants.TOKEN_EXPIRATION)
+}
+
+func RemoveCookie(c *gin.Context) {
+	setCookie(c, "", -1)
 }
