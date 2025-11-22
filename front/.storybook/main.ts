d2e7b5bb15234709c9e2df969b312efa1b16e1d9
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2025-11-22 15:55:23 +0100

    feat(storybook): add storybook and stories for TextInputAtom component

---
+++ front/.storybook/main.ts
@@ -0,0 +1,23 @@
+import remarkGfm from 'remark-gfm';
+import type { StorybookConfig } from 'storybook-react-rsbuild';
+
+const config: StorybookConfig = {
+  stories: ['../src/ui/**/*.mdx', '../src/ui/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
+  ],
+  framework: 'storybook-react-rsbuild',
+  rsbuildFinal: config => config,
+};
+
+export default config;
---
+++ front/.storybook/preview.ts
@@ -0,0 +1,16 @@
+// @ts-expect-error: side-effect import for global styles
+import '@Front/assets/css/index.ts';
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
