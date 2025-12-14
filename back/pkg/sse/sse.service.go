ee78d588ccb2e74d2bcc30c224ec3ff160d6370e
Nekeo <nekeo@zide.fr>
2025-12-14 22:56:43 +0100

    chore: remove logs

--- back/pkg/sse/sse.service.go
+++ back/pkg/sse/sse.service.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	"github.com/rs/zerolog/log"
 )
 
 type SSEClient struct {
@@ -68,7 +67,6 @@ func (s *SSEService) AddClient(clientID string, userId uuid.UUID, eventId uuid.U
 	}
 
 	s.clients[clientID] = client
-	log.Debug().Str("clientID", clientID).Str("userId", userId.String()).Str("eventId", eventId.String()).Msg("SSE client connected")
 
 	return client
 }
@@ -82,7 +80,6 @@ func (s *SSEService) RemoveClient(clientID string) {
 		client.Cancel()
 		close(client.Channel)
 		delete(s.clients, clientID)
-		log.Debug().Str("clientID", clientID).Msg("SSE client disconnected")
 	}
 }
 
@@ -91,17 +88,12 @@ func (s *SSEService) BroadcastSlotsUpdate(eventId uuid.UUID, slots []model.Slot)
 	s.mutex.RLock()
 	defer s.mutex.RUnlock()
 
-	log.Debug().Str("eventId", eventId.String()).Int("slotsCount", len(slots)).Msg("Starting to broadcast slot update")
-
 	message := SlotUpdateMessage(slots)
 
 	messageBytes, err := json.Marshal(message)
 	if err != nil {
-		log.Error().Err(err).Msg("Failed to marshal slot update message")
 		return
 	}
-
-	log.Debug().Str("messageJSON", string(messageBytes)).Msg("Marshaled SSE message")
 
 	var connectedClients []string
 	var sentCount int
@@ -109,26 +101,18 @@ func (s *SSEService) BroadcastSlotsUpdate(eventId uuid.UUID, slots []model.Slot)
 
 	for clientID, client := range s.clients {
 		totalClients++
-		if client.EventId == eventId {
-			log.Debug().Str("clientID", clientID).Str("userId", client.userId.String()).Msg("Sending message to client")
-			select {
-			case client.Channel <- messageBytes:
-				connectedClients = append(connectedClients, clientID)
-				sentCount++
-				log.Debug().Str("clientID", clientID).Msg("Message sent successfully to client")
-			case <-client.Context.Done():
-				// Client context is done, remove it
-				log.Warn().Str("clientID", clientID).Msg("Client context done, removing client")
-				go s.RemoveClient(clientID)
-			default:
-				log.Warn().Str("clientID", clientID).Msg("Client channel full, skipping message")
-			}
-		} else {
-			log.Debug().Str("clientID", clientID).Str("clientEventID", client.EventId.String()).Str("targetEventID", eventId.String()).Msg("Client not for this event, skipping")
+		if client.EventId != eventId {
+			continue
+		}
+		select {
+		case client.Channel <- messageBytes:
+			connectedClients = append(connectedClients, clientID)
+			sentCount++
+		case <-client.Context.Done():
+			// Client context is done, remove it
+			go s.RemoveClient(clientID)
 		}
 	}
-
-	log.Debug().Str("eventId", eventId.String()).Strs("clients", connectedClients).Int("sentCount", sentCount).Int("totalClients", totalClients).Msg("Broadcasted slot update to connected clients")
 }
 
 // GetConnectedClientsCount returns the number of connected clients for an event
@@ -153,23 +137,15 @@ func (s *SSEService) HandleSSEConnection(c *gin.Context, userId uuid.UUID, event
 	client := s.AddClient(clientID, userId, eventId, c.Request.Context())
 	defer s.RemoveClient(clientID)
 
-	log.Debug().Str("clientID", clientID).Str("userId", userId.String()).Str("eventId", eventId.String()).Msg("SSE client connection established")
-
 	// Listen for messages and client disconnect
 	for {
 		select {
 		case message := <-client.Channel:
-			log.Debug().Str("clientID", clientID).Str("message", string(message)).Msg("Sending SSE message to client")
-
 			fmt.Fprintf(c.Writer, "data: %s\n\n", string(message))
 			c.Writer.Flush()
-
-			log.Debug().Str("clientID", clientID).Msg("SSE message sent and flushed")
 		case <-client.Context.Done():
-			log.Debug().Str("clientID", clientID).Msg("SSE client context done")
 			return
 		case <-c.Request.Context().Done():
-			log.Debug().Str("clientID", clientID).Msg("SSE connection closed by client")
 			return
 		}
 	}
