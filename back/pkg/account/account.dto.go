144f0b05eb0b957a39d8d4a45a37fcfbedaf73f7
Nekeo <nekeo@zide.fr>
2025-12-15 09:43:48 +0100

    feat(back): remove username from register dto (#440)

--- back/pkg/account/account.dto.go
+++ back/pkg/account/account.dto.go
@@ -1,7 +1,6 @@
 package account
 
 type AccountCreateDto struct {
-	UserName string `json:"username" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
