1b8c66d56ca82ab3b059d616e617f0724076a1ea
Elisa Rogge <elisa.rogge59@gmail.com>
2025-12-17 15:05:35 +0100

    fix: lint error on test

--- front/src/ui/atoms/Inputs/CheckboxInputAtom/__tests__/CheckboxInputAtom.test.tsx
+++ front/src/ui/atoms/Inputs/CheckboxInputAtom/__tests__/CheckboxInputAtom.test.tsx
@@ -3,9 +3,10 @@ import { CheckboxInputAtom } from '../CheckboxInputAtom';
 
 describe('CheckboxInputAtom', () => {
   it('renders a checkbox input', () => {
-    render(<CheckboxInputAtom name='test' />);
+    render(<CheckboxInputAtom name="test" />);
     const input = screen.getByRole('checkbox');
     expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute('name', 'test');
     expect(input).toHaveClass('ds-checkbox-input-atom');
   });
 });
