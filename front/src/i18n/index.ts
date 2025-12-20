6710712ac20a2beed4ab44aa31cc4ac162d8d1af
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2025-12-20 17:38:53 +0100

    chore(i18n): update i18next resources structure for English localization
    Refactor the English localization resources to explicitly import
    individual JSON files instead of using a single object. This improves
    clarity and maintainability of the localization setup.

--- front/src/i18n/index.ts
+++ front/src/i18n/index.ts
@@ -1,11 +1,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { en } from './locales/en';
+import enAuthentication from './locales/en/authentication.json';
+import enDashboard from './locales/en/dashboard.json';
+import enError from './locales/en/error.json';
+import enSignUp from './locales/en/signUp.json';
+import enWelcome from './locales/en/welcome.json';
 
 // oxlint-disable-next-line no-named-as-default-member
 i18n.use(initReactI18next).init({
   resources: {
-    en,
+    en: {
+      authentication: enAuthentication,
+      dashboard: enDashboard,
+      error: enError,
+      signUp: enSignUp,
+      welcome: enWelcome,
+    },
   },
   lng: 'en',
   fallbackLng: 'en',
