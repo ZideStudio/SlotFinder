583eb9b9b6c2d33be9231164368a247ba50d0d1c
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2025-12-20 18:12:02 +0100

    feat(tests): add base Vitest configuration

--- front/vitest.browser.config.ts
+++ front/vitest.browser.config.ts
@@ -1,60 +1,19 @@
-import react from '@vitejs/plugin-react-swc';
 import { playwright } from '@vitest/browser-playwright';
-import { resolve } from 'path';
-import { loadEnv } from 'vite';
-import svgr from 'vite-plugin-svgr';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-// eslint-disable-next-line import/no-unresolved
 import { defineConfig } from 'vitest/config';
+import { getBaseConfig } from './config/vitest.base';
 
+// TODO: setup vitest browser with msw : https://mswjs.io/docs/recipes/vitest-browser-mode
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const base = getBaseConfig(mode);
   return {
-    plugins: [
-      react(),
-      viteTsconfigPaths({
-        projects: [resolve(__dirname, './tsconfig.test.json')],
-      }),
-      svgr(),
-    ],
-    envPrefix: env.ENV_PREFIX ?? 'FRONT_',
+    ...base,
     test: {
-      globals: true,
-      // TODO: setup vitest browser with msw : https://mswjs.io/docs/recipes/vitest-browser-mode
-      clearMocks: true,
-      css: false,
-      reporters: ['default', 'junit', 'vitest-sonar-reporter'],
-      outputFile: {
-        'vitest-sonar-reporter': 'sonar-report.xml',
-        junit: 'junit-report.xml',
-      },
+      ...base.test,
       include: ['src/**/*.browser.test.[jt]sx'],
-      maxWorkers: env.CI ? 2 : undefined,
-      coverage: {
-        enabled: env.CI === 'true',
-        reporter: ['lcovonly', 'html', 'text', 'text-summary'],
-        provider: 'v8',
-        lines: 80,
-        functions: 75,
-        branches: 80,
-        statements: 80,
-        include: ['src/**/*.[jt]s?(x)'],
-        exclude: [
-          'src/**/*.d.[jt]s?(x)',
-          'src/utils/tests/**/*.[jt]s?(x)',
-          'src/**/*.types.[jt]s?(x)',
-          'src/**/*.stories.[jt]s?(x)',
-          'src/**/*.(spec|test|steps).[jt]s?(x)',
-          'src/**/__tests__/**/*.[jt]s?(x)',
-          'src/i18n/**',
-          'src/routing/**',
-          'src/main.ts',
-        ],
-      },
+      exclude: undefined,
       browser: {
         enabled: true,
         provider: playwright(),
-        // https://vitest.dev/config/browser/playwright
         instances: [{ browser: 'chromium' }],
       },
     },
--- front/vitest.config.mts
+++ front/vitest.config.mts
@@ -1,58 +1,16 @@
-import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
-import { loadEnv } from 'vite';
-import svgr from 'vite-plugin-svgr';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-// eslint-disable-next-line import/no-unresolved
 import { defineConfig } from 'vitest/config';
+import { getBaseConfig } from './config/vitest.base';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
+  const base = getBaseConfig(mode);
   return {
-    plugins: [
-      react(),
-      viteTsconfigPaths({
-        projects: [resolve(__dirname, './tsconfig.test.json')],
-      }),
-      svgr(),
-    ],
-    envPrefix: env.ENV_PREFIX ?? 'FRONT_',
+    ...base,
     test: {
-      globals: true,
+      ...base.test,
       environment: 'jsdom',
       setupFiles: 'vitest.setup.ts',
-      clearMocks: true,
-      css: false,
-      reporters: ['default', 'junit', 'vitest-sonar-reporter'],
-      outputFile: {
-        'vitest-sonar-reporter': 'sonar-report.xml',
-        junit: 'junit-report.xml',
-      },
       include: ['src/**/*.(spec|test|steps).[jt]s?(x)'],
       exclude: ['src/**/*.browser.test.[jt]sx'],
-      maxWorkers: env.CI ? 2 : undefined,
-      coverage: {
-        enabled: env.CI === 'true',
-        reporter: ['lcovonly', 'html', 'text', 'text-summary'],
-        provider: 'v8',
-        lines: 80,
-        functions: 75,
-        branches: 80,
-        statements: 80,
-        include: ['src/**/*.[jt]s?(x)'],
-        exclude: [
-          'src/**/*.d.[jt]s?(x)',
-          'src/utils/tests/**/*.[jt]s?(x)',
-          'src/**/*.types.[jt]s?(x)',
-          'src/**/*.stories.[jt]s?(x)',
-          'src/**/*.(spec|test|steps).[jt]s?(x)',
-          'src/**/__tests__/**/*.[jt]s?(x)',
-          'src/i18n/**',
-          'src/routing/**',
-          'src/main.ts',
-        ],
-      },
     },
   };
 });
