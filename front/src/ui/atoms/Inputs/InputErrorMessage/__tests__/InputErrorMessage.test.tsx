932c58ccf0895c1e620c00f1c2709c4e747ae06f
Elisa Rogge <elisa.rogge59@gmail.com>
2026-01-03 19:06:30 +0100

    fix: prettier errors

--- front/src/ui/atoms/Inputs/InputErrorMessage/__tests__/InputErrorMessage.test.tsx
+++ front/src/ui/atoms/Inputs/InputErrorMessage/__tests__/InputErrorMessage.test.tsx
@@ -2,18 +2,17 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { InputErrorMessage } from '../InputErrorMessage';
 
-describe('', () => {
+describe('InputErrorMessage', () => {
   it('renders an error message with correct message', () => {
-    render(<InputErrorMessage message="test-input"></InputErrorMessage>);
+    render(<InputErrorMessage message="test-input" />);
     const errorMessage = screen.getByText('test-input');
     expect(errorMessage).toBeInTheDocument();
     expect(errorMessage).toHaveTextContent('test-input');
   });
+
   it('applies custom id when provided', () => {
-    render(<InputErrorMessage message="test-input" id="test-id"></InputErrorMessage>);
+    render(<InputErrorMessage message="test-input" id="test-id" />);
     const errorMessage = screen.getByText('test-input');
     expect(errorMessage).toHaveAttribute('id', 'test-id');
   });
-  
-
 });
