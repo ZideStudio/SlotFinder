a6a404dba66e4e9a968231acb9b26ba6113f7802
Nekeo <nekeo@zide.fr>
2026-01-06 22:36:33 +0100

    fix: remove test expiration

--- back/commons/constants/token_expiration.constant.go
+++ back/commons/constants/token_expiration.constant.go
@@ -2,6 +2,6 @@ package constants
 
 import "time"
 
-const ACCESS_TOKEN_EXPIRATION = 10 * time.Second
+const ACCESS_TOKEN_EXPIRATION = 15 * time.Minute
 const REFRESH_TOKEN_EXPIRATION = 168 * time.Hour
 const TOKEN_RENEWAL_THRESHOLD_EXPIRATION = 5 * time.Minute
