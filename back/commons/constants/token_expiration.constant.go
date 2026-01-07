4fa29287db893ab5f5e608ab52b479ba4008d80c
Nekeo <nekeo@zide.fr>
2026-01-07 10:44:02 +0100

    feat: improve cookie security

--- back/commons/constants/token_expiration.constant.go
+++ back/commons/constants/token_expiration.constant.go
@@ -2,6 +2,6 @@ package constants
 
 import "time"
 
-const ACCESS_TOKEN_EXPIRATION = 15 * time.Minute
+const ACCESS_TOKEN_EXPIRATION = 10 * time.Second
 const REFRESH_TOKEN_EXPIRATION = 8760 * time.Hour // 1 year
 const TOKEN_RENEWAL_THRESHOLD_EXPIRATION = 5 * time.Minute
