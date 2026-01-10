6493d74c932153104ff3646f45feaaad18eb20ba
Nekeo <nekeo@zide.fr>
2026-01-10 18:06:29 +0100

    feat(back): require accepted terms to use api

--- back/pkg/account/account.dto.go
+++ back/pkg/account/account.dto.go
@@ -3,17 +3,21 @@ package account
 import "app/commons/constants"
 
 type AccountCreateDto struct {
-	Email    string                    `json:"email" binding:"required,email,min=6,max=320"`
-	Password string                    `json:"password" binding:"required,max=100"`
-	Language constants.AccountLanguage `json:"language" binding:"required,oneof=en fr"`
+	Email         string                    `json:"email" binding:"required,email,min=6,max=320"`
+	Password      string                    `json:"password" binding:"required,max=100"`
+	Language      constants.AccountLanguage `json:"language" binding:"required,oneof=en fr"`
+	TermsAccepted bool                      `json:"termsAccepted" binding:"required,eq=true"`
+	TermsVersion  string                    `json:"termsVersion" binding:"required"`
 }
 
 type AccountUpdateDto struct {
-	UserName *string                    `json:"username" binding:"omitempty,min=3,max=30"`
-	Email    *string                    `json:"email" binding:"omitempty,min=6,max=320"`
-	Password *string                    `json:"password" binding:"omitempty,max=100"`
-	Color    *string                    `json:"color"`
-	Language *constants.AccountLanguage `json:"language" binding:"omitempty,oneof=en fr"`
+	UserName      *string                    `json:"username" binding:"omitempty,min=3,max=30"`
+	Email         *string                    `json:"email" binding:"omitempty,min=6,max=320"`
+	Password      *string                    `json:"password" binding:"omitempty,max=100"`
+	Color         *string                    `json:"color"`
+	Language      *constants.AccountLanguage `json:"language" binding:"omitempty,oneof=en fr"`
+	TermsAccepted *bool                      `json:"termsAccepted" binding:"omitempty,eq=true"`
+	TermsVersion  *string                    `json:"termsVersion"`
 }
 
 type ForgotPasswordDto struct {
