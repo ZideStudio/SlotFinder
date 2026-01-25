1d36a44319a15f545c3fdb612bd83a3a8c7830d9
Nekeo <nekeo@zide.fr>
2026-01-25 18:14:51 +0100

    feat: add dev container config

--- front/src/api/authentication/signUpApi.ts
+++ front/src/api/authentication/signUpApi.ts
@@ -1,12 +1,21 @@
-import type { SignUpErrorCodeType, SignUpRequestBodyType, SignUpResponseType } from "@Front/types/Authentication/signUp/signUp.types";
-import { SignUpErrorResponse } from "@Front/types/Authentication/signUp/SignUpErrorResponse";
-import { METHODS } from "../constant";
-import { fetchApi } from "../fetchApi";
+import type {
+  SignUpErrorCodeType,
+  SignUpRequestBodyType,
+  SignUpResponseType,
+} from '@Front/types/Authentication/signUp/signUp.types';
+import { SignUpErrorResponse } from '@Front/types/Authentication/signUp/SignUpErrorResponse';
+import { METHODS } from '../constant';
+import { fetchApi } from '../fetchApi';
 
-export const signUpApi = ({ username, email, password, language }: SignUpRequestBodyType): Promise<SignUpResponseType> => {
+export const signUpApi = ({
+  username,
+  email,
+  password,
+  language,
+}: SignUpRequestBodyType): Promise<SignUpResponseType> => {
   const body: SignUpRequestBodyType = { username, email, password, language };
 
-  return fetchApi<SignUpResponseType, SignUpErrorCodeType>({
+         return fetchApi<SignUpResponseType, SignUpErrorCodeType>({
     path: `${import.meta.env.FRONT_BACKEND_URL}/v1/account`,
     method: METHODS.post,
     data: body,
