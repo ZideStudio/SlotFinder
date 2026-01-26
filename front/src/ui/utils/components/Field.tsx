7ec275d5acd1fdf94765ac83395f57e51bacb418
Elisa Rogge <elisa.rogge59@gmail.com>
2026-01-26 22:08:48 +0100

    fix: field props

--- front/src/ui/utils/components/Field.tsx
+++ front/src/ui/utils/components/Field.tsx
@@ -1,20 +1,20 @@
 import { InputErrorMessage } from "@Front/ui/atoms/Inputs/InputErrorMessage/InputErrorMessage";
 import { LabelInput } from "@Front/ui/atoms/Inputs/LabelInput/LabelInput";
 import { getClassName } from "@Front/utils/getClassName";
-import { ComponentProps, useId, ReactNode } from "react";
+import { ElementType, useId, ReactNode, ComponentProps } from "react";
 import './Field.scss';
 
-type FieldProps = Omit<ComponentProps<'input'>, 'id'> & {
-    input: React.ComponentType<any>;
+type FieldProps<ComponentType extends ElementType> = {
+    input: ComponentType;
     id?: string;
     label?: ReactNode;
     error?: ReactNode;
     defaultClassName?: string;
     required?: boolean;
     className?: string;
-}
+} & Omit<ComponentProps<ComponentType>, 'id' | 'aria-describedby' | 'aria-invalid' | 'required'>;
 
-export const Field = ({ input: Input, id, error, label, required, className, defaultClassName = "ds-field", ...props }: FieldProps) => {
+export const Field = <ComponentType extends ElementType>({ input: Input, id, error, label, required, className, defaultClassName = "ds-field", ...props }: FieldProps<ComponentType>) => {
   const generatedId = useId();
   const inputId = id || generatedId;
   const errorId = `${inputId}-error`;
@@ -24,12 +24,14 @@ export const Field = ({ input: Input, id, error, label, required, className, def
     className,
   });
 
+  const Component = Input as React.ElementType;
+
   return (
     <div className={parentClassName}>
       <LabelInput inputId={inputId} required={required}>
         {label}
       </LabelInput>
-      <Input
+      <Component
         id={inputId}
         aria-describedby={error ? errorId : undefined}
         aria-invalid={Boolean(error)}
