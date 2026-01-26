b5771ad6c5dc6534eabe056bfdc3afd5f593959b
sartek430 <macogneur430@gmail.comm>
2026-01-26 14:01:35 +0100

    feat(tag): update tag component to use color prop and remove variants
    Refactor the Tag component to replace variant props with a color prop for styling.
    Update related stories and tests to reflect this change, ensuring consistent usage of filled and outlined appearances.

--- front/src/ui/Tag/Tag.tsx
+++ front/src/ui/Tag/Tag.tsx
@@ -2,25 +2,27 @@ import { getClassName } from '@Front/utils/getClassName';
 import './Tag.scss';
 import { type ReactNode } from 'react';
 
-export type TagVariant ='default' |'inDecision' | 'comingSoon' | 'finished';
 export type TagAppearance = 'filled' | 'outlined';
 
 export type TagProps = {
-  variant?: TagVariant;
+  color: string;
   className?: string;
   children: ReactNode;
   appearance?: TagAppearance;
 };
 
-export const Tag = ({ className, children, variant = 'default', appearance = 'filled' }: TagProps) => {
+export const Tag = ({ className, children, color, appearance = 'filled' }: TagProps) => {
   const parentClassName = getClassName({
     defaultClassName: 'ds-tag',
     className,
-    modifiers: [variant, appearance].filter(Boolean) as string[],
+    modifiers: [appearance].filter(Boolean) as string[],
   });
 
   return (
-    <span className={parentClassName}>
+    <span
+      className={parentClassName}
+      style={{ backgroundColor: appearance === 'filled' ? color : 'transparent', borderColor: color }}
+    >
       <span className="ds-tag__content" title={typeof children === 'string' ? children : undefined}>
         {children}
       </span>
