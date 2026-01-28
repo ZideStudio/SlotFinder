dcfcbf506ef5785d34a448f2f2e8033e3870e429
Elisa Rogge <elisa.rogge59@gmail.com>
2026-01-28 13:15:31 +0100

    fix: field and textinput props

--- front/src/ui/molecules/Inputs/TextInput/TextInput.tsx
+++ front/src/ui/molecules/Inputs/TextInput/TextInput.tsx
@@ -1,6 +1,6 @@
-import { TextInputAtom } from '@Front/ui/atoms/Inputs/TextInputAtom/TextInputAtom';
+import { TextInputAtom, TextInputAtomProps } from '@Front/ui/atoms/Inputs/TextInputAtom/TextInputAtom';
 import { Field } from '@Front/ui/utils/components/Field';
 
-export const TextInput = ({ ...textInputAtomProps }) => {
-  return <Field input={TextInputAtom} {...textInputAtomProps} />;
+export const TextInput = (props: TextInputAtomProps) => {
+  return <Field input={TextInputAtom} {...props} />;
 };
