a17be9b228219a297ecb81eaca7f130322edf6ef
Nekeo <nekeo@zide.fr>
2026-02-21 13:58:30 +0100

    Revert package and signUpApi changes

--- front/src/api/authentication/signUpApi.ts
+++ front/src/api/authentication/signUpApi.ts
@@ -1,18 +1,9 @@
-import type {
-  SignUpErrorCodeType,
-  SignUpRequestBodyType,
-  SignUpResponseType,
-} from '@Front/types/Authentication/signUp/signUp.types';
-import { SignUpErrorResponse } from '@Front/types/Authentication/signUp/SignUpErrorResponse';
-import { METHODS } from '../constant';
-import { fetchApi } from '../fetchApi';
+import type { SignUpErrorCodeType, SignUpRequestBodyType, SignUpResponseType } from "@Front/types/Authentication/signUp/signUp.types";
+import { SignUpErrorResponse } from "@Front/types/Authentication/signUp/SignUpErrorResponse";
+import { METHODS } from "../constant";
+import { fetchApi } from "../fetchApi";
 
-export const signUpApi = ({
-  username,
-  email,
-  password,
-  language,
-}: SignUpRequestBodyType): Promise<SignUpResponseType> => {
+export const signUpApi = ({ username, email, password, language }: SignUpRequestBodyType): Promise<SignUpResponseType> => {
   const body: SignUpRequestBodyType = { username, email, password, language };
 
   return fetchApi<SignUpResponseType, SignUpErrorCodeType>({
