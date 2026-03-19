00fb0ce7dd2fdfb815397cccaa7b638a90a8f062
Elisa Rogge <elisa.rogge59@gmail.com>
2026-03-19 18:48:59 +0100

    fix: remove value check on test

--- front/src/ui/atoms/Inputs/DateInputAtom/__tests__/DateInputAtom.test.tsx
+++ front/src/ui/atoms/Inputs/DateInputAtom/__tests__/DateInputAtom.test.tsx
@@ -6,7 +6,6 @@ describe('DateInputAtom', () => {
     render(<DateInputAtom name="date-input" value="2026-01-01" />);
     const input = screen.getByDisplayValue('2026-01-01');
     expect(input).toBeInTheDocument();
-    expect(input).toHaveAttribute('value', '2026-01-01');
     expect(input).toHaveAttribute('type', 'date');
     expect(input).toHaveAttribute('name', 'date-input');
     expect(input).toHaveClass('ds-date-input-atom');
