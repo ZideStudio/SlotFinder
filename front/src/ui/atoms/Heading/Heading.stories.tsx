042f7ac61d66eddd48e44c3c86a1c7181d095779
VAYR_hugo <77483277+sartek430@users.noreply.github.com>
2026-03-31 00:36:39 +0200

    feat(heading): add reusable heading component with styles and tests (#779)
    Co-authored-by: sartek430 <macogneur430@gmail.comm>

---
+++ front/src/ui/atoms/Heading/Heading.stories.tsx
@@ -0,0 +1,31 @@
+// oxlint-disable no-magic-numbers
+import type { Meta, StoryObj } from 'storybook-react-rsbuild';
+
+import { Heading } from './Heading';
+
+const meta = {
+  component: Heading,
+  title: 'Atoms/Heading',
+  args: {
+    level: 1,
+  },
+  argTypes: {
+    level: {
+      control: {
+        type: 'select',
+        options: [1, 2, 3],
+      },
+    },
+  },
+} satisfies Meta<typeof Heading>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'Heading 1',
+    className: 'custom-class',
+  },
+};
---
+++ front/src/ui/atoms/Heading/Heading.tsx
@@ -0,0 +1,23 @@
+// oxlint-disable react/jsx-props-no-spreading
+import { getClassName } from '@Front/utils/getClassName';
+import type { HTMLAttributes, ReactNode } from 'react';
+import './Heading.scss';
+
+type HeadingTag = 'h1' | 'h2' | 'h3';
+type HeadingProps = HTMLAttributes<HTMLHeadingElement> & {
+  // oxlint-disable-next-line no-magic-numbers
+  level: 1 | 2 | 3;
+  children: ReactNode;
+};
+
+export const Heading = ({ level, className, ...props }: HeadingProps) => {
+  const parentClassName = getClassName({
+    defaultClassName: 'ds-heading',
+    className,
+    modifiers: [`level-${level}`],
+  });
+
+  const Tag = `h${level}` satisfies HeadingTag;
+
+  return <Tag {...props} className={parentClassName} />;
+};
