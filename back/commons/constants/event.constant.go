6bc91056cee400419047849734eb4c6a7f5b78c8
Nekeo <nekeo@zide.fr>
2026-04-15 22:13:43 +0200

    feat(back): make status property in db as enum instead of string (#872)

--- back/commons/constants/event.constant.go
+++ back/commons/constants/event.constant.go
@@ -1,5 +1,10 @@
 package constants
 
+import (
+	"database/sql/driver"
+	"fmt"
+)
+
 type EventStatus string
 
 const (
@@ -7,3 +12,27 @@ const (
 	EVENT_STATUS_UPCOMING    EventStatus = "UPCOMING"
 	EVENT_STATUS_FINISHED    EventStatus = "FINISHED"
 )
+
+var EventStatuses = []EventStatus{EVENT_STATUS_IN_DECISION, EVENT_STATUS_UPCOMING, EVENT_STATUS_FINISHED}
+
+func (ct *EventStatus) Scan(value any) error {
+	if value == nil {
+		*ct = ""
+		return nil
+	}
+
+	switch v := value.(type) {
+	case []byte:
+		*ct = EventStatus(string(v))
+		return nil
+	case string:
+		*ct = EventStatus(v)
+		return nil
+	default:
+		return fmt.Errorf("cannot scan %T into EventStatus", value)
+	}
+}
+
+func (ct EventStatus) Value() (driver.Value, error) {
+	return string(ct), nil
+}
