ba3c3381423b087b7f4c0bb33ff1ea9b4d572342
Elisa ROGGE <elisa.rogge.pro@gmail.com>
2026-05-20 14:53:10 +0200

    feat: textarea field component (#968)

---
+++ front/src/components/TextareaField/TextareaField.tsx
@@ -0,0 +1,7 @@
+import { Field } from '@Front/components/Field/Field';
+import { TextareaInput } from '@Front/ui/molecules/Inputs/TextareaInput/TextareaInput';
+import { type ComponentProps } from 'react';
+
+type TextareaInputProps = Omit<ComponentProps<typeof TextareaInput>, 'error'>;
+
+export const TextareaField = (props: TextareaInputProps) => <Field input={TextareaInput} {...props} />;
