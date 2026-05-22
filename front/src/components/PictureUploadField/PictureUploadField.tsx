54e8f93e10ca5e7cf504da155f59883fc5ba4277
Elisa ROGGE <elisa.rogge.pro@gmail.com>
2026-05-22 13:06:24 +0200

    feat: picture upload field component (#972)

---
+++ front/src/components/PictureUploadField/PictureUploadField.tsx
@@ -0,0 +1,7 @@
+import { Field } from '@Front/components/Field/Field';
+import { PictureUploadInput } from '@Front/ui/molecules/Inputs/PictureUploadInput/PictureUploadInput';
+import { type ComponentProps } from 'react';
+
+type PictureUploadInputProps = Omit<ComponentProps<typeof PictureUploadInput>, 'error'>;
+
+export const PictureUploadField = (props: PictureUploadInputProps) => <Field input={PictureUploadInput} {...props} />;
