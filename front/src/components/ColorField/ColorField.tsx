3f1907d2942f786c8d0245607439850990384f42
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2026-05-25 15:06:04 +0000

    refactor(front): simplify ColorField component rendering logic

--- front/src/components/ColorField/ColorField.tsx
+++ front/src/components/ColorField/ColorField.tsx
@@ -1,6 +1,6 @@
 import { ColorInput } from '@Front/ui/molecules/Inputs/ColorInput/ColorInput';
 import { type ComponentProps } from 'react';
-import { Controller, get, useFormContext, type FieldValues, type Path } from 'react-hook-form';
+import { Controller, useFormContext, type FieldValues, type Path } from 'react-hook-form';
 
 type ColorFieldProps<FormValues extends FieldValues = FieldValues> = Omit<
   ComponentProps<typeof ColorInput>,
@@ -13,17 +13,13 @@ export const ColorField = <FormValues extends FieldValues = FieldValues>({
   name,
   ...props
 }: ColorFieldProps<FormValues>) => {
-  const {
-    control,
-    formState: { errors },
-  } = useFormContext<FormValues>();
-  const fieldError = get(errors, name);
+  const { control } = useFormContext<FormValues>();
 
   return (
     <Controller
       name={name}
       control={control}
-      render={({ field: fieldProps }) => <ColorInput {...props} {...fieldProps} error={fieldError?.message} />}
+      render={({ field, fieldState: { error } }) => <ColorInput {...props} {...field} error={error?.message} />}
     />
   );
 };
