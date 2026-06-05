6ff5cee812adbbf05055b79f1068599a5748c5ee
sartek430 <macogneur430@gmail.comm>
2026-06-05 09:03:14 +0000

    feat(tests): wrap OAuth component in LoaderProvider for loading state

--- front/src/pages/Home/Dashboard/Dashboard.tsx
+++ front/src/pages/Home/Dashboard/Dashboard.tsx
@@ -2,29 +2,15 @@ import { Header } from "@Front/components/Layout/Header/Header";
 import { useTranslation } from "react-i18next";
 
 import "./Dashboard.scss";
-import { Button } from "@Front/ui/molecules/Button/Button";
-import { useLoader } from "@Front/hooks/useLoader";
 
 export const Dashboard = () => {
   const { t } = useTranslation("dashboard");
-  const { showLoader, hideLoader } = useLoader();
-
-  const displayLoader = () => {
-    showLoader();
-    setTimeout(() => {
-      hideLoader();
-    }, 2000);
-  };
 
   return (
     <div className="dashboard">
       <Header ignoreRouteHideHeader className="dashboard__header" />
       <div className="dashboard__content">
         <h1>{t("title")}</h1>
-
-        <Button variant="primary" onClick={displayLoader}>
-          Click Me
-        </Button>
       </div>
     </div>
   );
