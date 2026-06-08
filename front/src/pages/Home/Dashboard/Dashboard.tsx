be6289bd5ef77640ac3e03c470ccc398a7648bb9
sartek430 <macogneur430@gmail.comm>
2026-06-08 11:47:44 +0000

    feat(dashboard): enhance dashboard layout and structure
    - Added flex properties to dashboard content for better layout.
    - Updated class names for consistency in dashboard content structure.

--- front/src/pages/Home/Dashboard/Dashboard.tsx
+++ front/src/pages/Home/Dashboard/Dashboard.tsx
@@ -1,21 +1,20 @@
 import { Header } from "@Front/components/Layout/Header/Header";
-import { useTranslation } from "react-i18next";
 
 import "./Dashboard.scss";
+import { Heading } from "@Front/ui/atoms/Heading/Heading";
+import { Button } from "@Front/ui/molecules/Button/Button";
 
-export const Dashboard = () => {
-  const { t } = useTranslation("dashboard");
-
-  return (
-    <div className="dashboard">
-      <Header ignoreRouteHideHeader className="dashboard__header" />
-      <div className="dashboard__content">
+export const Dashboard = () => (
+  <div className="dashboard">
+    <Header ignoreRouteHideHeader className="dashboard__header" />
+    <div className="dashboard__content">
+      <div className="dashboard__content--header">
         <Heading level={1}>My events</Heading>
 
         <Button className="dashboard__button">Create an event</Button>
       </div>
 
-      <div className="dashboard__no-events">No events here</div>
+      <div className="dashboard__content--no-events">No events here</div>
     </div>
-  );
-};
+  </div>
+);
