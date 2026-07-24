9d8310f60d5e85286a6f9c7c8e0fdbec03ee3dd3
sartek430 <macogneur430@gmail.comm>
2026-07-24 16:37:08 +0000

    feat(oauth): refactor OAuth page component to use native elements

--- front/src/pages/Authentication/OAuth/OAuth.tsx
+++ front/src/pages/Authentication/OAuth/OAuth.tsx
@@ -1,41 +1,30 @@
 import { useTranslation } from "react-i18next";
-
+import "./OAuth.scss";
 import { useOAuth } from "./useOAuth";
-import { Heading } from "@Front/ui/atoms/Heading/Heading";
-import { Link } from "@Front/ui/atoms/Link/Link";
-
-import "./OAuth.css";
 
 export const OAuth = () => {
   const { t } = useTranslation("authentication");
   const { oAuthProviders } = useOAuth();
 
   return (
-    <Grid
-      component="nav"
-      container
-      colSpan={{ "desktop-small": 4, tablet: 4, mobile: 4 }}
-      colStart={{ "desktop-small": 5, tablet: 3, mobile: 1 }}
-      aria-labelledby="oauth-provider-heading"
-      className="oauth-nav"
-    >
-      <Heading level={2} id="oauth-provider-heading">
+    <nav aria-labelledby="oauth-provider-heading" className="oauth-nav">
+      <h2 className="oauth-nav__heading" id="oauth-provider-heading">
         {t("signInWithProvider")}
-      </Heading>
+      </h2>
       <ul className="oauth-nav__list">
         {oAuthProviders.map((provider) => (
           <li key={provider.label}>
-            <Link
+            <a
               href={provider.href}
               aria-label={t(provider.ariaLabel)}
-              openInNewTab
+              rel="noopener noreferrer"
             >
               {provider.icon}
               <span>{provider.label}</span>
-            </Link>
+            </a>
           </li>
         ))}
       </ul>
-    </Grid>
+    </nav>
   );
 };
