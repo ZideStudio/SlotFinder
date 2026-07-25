bcebe9867fdce7aa29ef8214380f00d88917a786
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2026-07-25 15:01:47 +0000

    feat(CardPage): add Slot Finder logo to card page
    Include a logo link that navigates to the home route. The logo is conditionally rendered based on the isIconDisplayed prop. Update styles to accommodate the new logo.

--- front/src/components/CardPage/CardPage.tsx
+++ front/src/components/CardPage/CardPage.tsx
@@ -1,12 +1,16 @@
+import { appRoutes } from "@Front/routing/appRoutes";
 import { Card } from "@Front/ui/atoms/Card/Card";
 import { Heading } from "@Front/ui/atoms/Heading/Heading";
 import { getClassName } from "@Front/utils/getClassName";
 import type { ComponentProps, ReactNode } from "react";
+import { NavLink } from "react-router";
+import logo from "../../../public/assets/black_logo_without_background.png";
 
 import "./CardPage.scss";
 
 type CardProps = {
   title: ReactNode;
+  isIconDisplayed?: boolean;
   children: ReactNode;
 } & ComponentProps<"section">;
 
@@ -14,6 +18,7 @@ const defaultClassName = "card-page";
 
 export const CardPage = ({
   title,
+  isIconDisplayed = true,
   className,
   children,
   ...props
@@ -25,6 +30,11 @@ export const CardPage = ({
 
   return (
     <section className={parentClassName} {...props}>
+      {isIconDisplayed && (
+        <NavLink to={appRoutes.home()} className="card-page__logo">
+          <img src={logo} alt="Slot Finder logo" />
+        </NavLink>
+      )}
       <Heading level={1} className={`${defaultClassName}__title`}>
         {title}
       </Heading>
