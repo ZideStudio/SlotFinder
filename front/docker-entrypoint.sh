5baa087009b8096072560992268b4d66b32e36da
Guillaume_KESTEMAN <81412571+GuillaumeKESTEMAN@users.noreply.github.com>
2026-08-01 12:20:57 +0200

    chore(docker): add .dockerignore and optimize Dockerfile for build (#1186)

--- front/docker-entrypoint.sh
+++ front/docker-entrypoint.sh
@@ -5,5 +5,9 @@ set -e
 echo "Running npm install..."
 npm install
 
+# Generate i18next resources for TypeScript
+echo "Generating i18next resources for TypeScript..."
+npm run i18next-resources-for-ts
+
 # Execute the command passed to the container
 exec "$@"
