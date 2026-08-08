071ccb697d7c1aacc66f13737ba22ee4deb90589
Nekeo <nekeo@zide.fr>
2026-08-08 15:42:13 +0200

    test: cover testutils/jwt.go

---
+++ back/testutils/jwt_test.go
@@ -0,0 +1,103 @@
+package testutils
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFileExists_ExistingFile(t *testing.T) {
+	path := filepath.Join(t.TempDir(), "some-file")
+	require.NoError(t, os.WriteFile(path, []byte("data"), 0o600))
+
+	assert.True(t, fileExists(path))
+}
+
+func TestFileExists_MissingPath(t *testing.T) {
+	assert.False(t, fileExists(filepath.Join(t.TempDir(), "does-not-exist")))
+}
+
+func TestFileExists_Directory(t *testing.T) {
+	assert.False(t, fileExists(t.TempDir()))
+}
+
+func readRSAKeyPair(t *testing.T, privateKeyPath, publicKeyPath string) {
+	t.Helper()
+
+	privateKeyPem, err := os.ReadFile(privateKeyPath)
+	require.NoError(t, err)
+	privateBlock, _ := pem.Decode(privateKeyPem)
+	require.NotNil(t, privateBlock, "private key file should contain a valid PEM block")
+	privateKey, err := x509.ParsePKCS1PrivateKey(privateBlock.Bytes)
+	require.NoError(t, err)
+
+	publicKeyPem, err := os.ReadFile(publicKeyPath)
+	require.NoError(t, err)
+	publicBlock, _ := pem.Decode(publicKeyPem)
+	require.NotNil(t, publicBlock, "public key file should contain a valid PEM block")
+	publicKeyAny, err := x509.ParsePKIXPublicKey(publicBlock.Bytes)
+	require.NoError(t, err)
+	publicKey, ok := publicKeyAny.(*rsa.PublicKey)
+	require.True(t, ok, "expected an RSA public key")
+
+	assert.True(t, privateKey.PublicKey.Equal(publicKey), "public key should correspond to the generated private key")
+}
+
+func TestEnsureTestJWTKeyPair_GeneratesValidKeyPair(t *testing.T) {
+	dir := t.TempDir()
+	privateKeyPath := filepath.Join(dir, "private.pem")
+	publicKeyPath := filepath.Join(dir, "public.pem")
+
+	require.NoError(t, EnsureTestJWTKeyPair(privateKeyPath, publicKeyPath))
+
+	assert.True(t, fileExists(privateKeyPath))
+	assert.True(t, fileExists(publicKeyPath))
+	readRSAKeyPair(t, privateKeyPath, publicKeyPath)
+}
+
+func TestEnsureTestJWTKeyPair_AlreadyExists_NoOp(t *testing.T) {
+	dir := t.TempDir()
+	privateKeyPath := filepath.Join(dir, "private.pem")
+	publicKeyPath := filepath.Join(dir, "public.pem")
+
+	require.NoError(t, EnsureTestJWTKeyPair(privateKeyPath, publicKeyPath))
+	privateBefore, err := os.ReadFile(privateKeyPath)
+	require.NoError(t, err)
+	publicBefore, err := os.ReadFile(publicKeyPath)
+	require.NoError(t, err)
+
+	require.NoError(t, EnsureTestJWTKeyPair(privateKeyPath, publicKeyPath))
+
+	privateAfter, err := os.ReadFile(privateKeyPath)
+	require.NoError(t, err)
+	publicAfter, err := os.ReadFile(publicKeyPath)
+	require.NoError(t, err)
+
+	assert.Equal(t, privateBefore, privateAfter, "an existing key pair should not be regenerated")
+	assert.Equal(t, publicBefore, publicAfter, "an existing key pair should not be regenerated")
+}
+
+func TestEnsureTestJWTKeyPair_OnlyPrivateKeyPresent_RegeneratesPair(t *testing.T) {
+	dir := t.TempDir()
+	privateKeyPath := filepath.Join(dir, "private.pem")
+	publicKeyPath := filepath.Join(dir, "public.pem")
+
+	require.NoError(t, EnsureTestJWTKeyPair(privateKeyPath, publicKeyPath))
+	privateBefore, err := os.ReadFile(privateKeyPath)
+	require.NoError(t, err)
+	require.NoError(t, os.Remove(publicKeyPath))
+
+	require.NoError(t, EnsureTestJWTKeyPair(privateKeyPath, publicKeyPath))
+
+	assert.True(t, fileExists(publicKeyPath), "the missing public key should be regenerated")
+	privateAfter, err := os.ReadFile(privateKeyPath)
+	require.NoError(t, err)
+	assert.NotEqual(t, privateBefore, privateAfter, "a fresh pair should be generated when only one of the two files is present")
+	readRSAKeyPair(t, privateKeyPath, publicKeyPath)
+}
