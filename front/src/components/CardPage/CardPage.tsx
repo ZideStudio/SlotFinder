760717a2881bb825e6bc7ac710f6d641ac02c113
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2026-08-08 16:03:54 +0000

    style(CardPage): update card component styles for responsiveness

--- front/src/components/CardPage/CardPage.tsx
+++ front/src/components/CardPage/CardPage.tsx
@@ -43,7 +43,7 @@ export const CardPage = ({
       <Heading level={1} className={`${defaultClassName}__title`}>
         {title}
       </Heading>
-      <Card borderWeight="bold">{children}</Card>
+      <Card className="card-page__card" borderWeight="bold">{children}</Card>
     </section>
   );
 };
