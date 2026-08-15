5c344521a95a886a5640b8ab93ade1a0c028fd69
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2026-08-15 09:30:12 +0000

    feat(picture-upload-input): add previewVariant prop for image preview styles
    Introduce a new prop `previewVariant` to allow selection between
    default and rounded image preview styles. Update styles and tests
    to accommodate this new feature.

--- front/src/ui/molecules/Inputs/PictureUploadInput/PictureUploadInput.stories.tsx
+++ front/src/ui/molecules/Inputs/PictureUploadInput/PictureUploadInput.stories.tsx
@@ -10,6 +10,9 @@ const meta = {
     name: "picture-upload-input",
     required: false,
     previewText: "Preview",
+    previewVariant: "default",
+    defaultPreviewUrl:
+      "https://avatars.githubusercontent.com/u/152389914?s=60&v=4",
     className: "custom-class",
   },
   argTypes: {
@@ -27,6 +30,18 @@ const meta = {
 export default meta;
 
 export const Default: StoryObj<typeof meta> = {};
+
+export const DefaultWithoutDefaultPreview: StoryObj<typeof meta> = {
+  args: {
+    defaultPreviewUrl: undefined,
+  },
+};
+
+export const WithRoundedPreview: StoryObj<typeof meta> = {
+  args: {
+    previewVariant: "rounded",
+  },
+};
 
 export const Invalid: StoryObj<typeof meta> = {
   args: {
