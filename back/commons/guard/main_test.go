a0e8d8f05a795e75ec23cd4ab757703b92daf1c3
Nekeo <nekeo@zide.fr>
2026-08-15 13:17:30 +0200

    test: revert guard LoadTestEnv, ORIGIN mismatch broke CORS test

--- back/commons/guard/main_test.go
+++ back/commons/guard/main_test.go
@@ -8,7 +8,9 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	testutils.LoadTestEnv()
+	if os.Getenv("DB_PORT") == "" {
+		_ = os.Setenv("DB_PORT", "5432")
+	}
 	testutils.EnsureTestAuthEnv()
 	if os.Getenv("ORIGIN") == "" {
 		_ = os.Setenv("ORIGIN", "https://slotfinder.test")
