1f4c8a84aa4622f91186ee5bf9c740c8fb557e0d
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2026-09-20 15:39:19 +0000

    chore(vitest): remove publicDir from browser config

--- front/config/vitest/vitest.browser.config.ts
+++ front/config/vitest/vitest.browser.config.ts
@@ -6,7 +6,6 @@ export default defineConfig(({ mode }) => {
   const base = getBaseConfig(mode);
   return {
     ...base,
-    publicDir: new URL("../../public", import.meta.url).pathname,
     test: {
       ...base.test,
       name: "browser",
